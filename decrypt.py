eb8465391f3c8d3a20de28d504582de601f60da7
Phlex <flynnacworth1@hotmail.com>
2015-03-30 01:22:18 +0100

    modified prompt strings

--- decrypt.py
+++ decrypt.py
@@ -36,13 +36,14 @@ def main():
 		message = f.read()
 		f.close()
 	else:
-		message = raw_input("Enter the message motherfucker: ")
+		message = raw_input("Enter the message: ")
 	
-	shift = int(raw_input("Enter the shift value motherfucker: "))
+	shift1 = int(raw_input("Enter the first shift value: "))
+	shift2 = int(raw_input("Enter the second shift value: "))
 	key, message = split_message(message)
-	real_key = caesar_decrypt(key, shift)
+	real_key = caesar_decrypt(key, shift2)
 	message = otp_decrypt(real_key, message)
-	print caesar_decrypt(message, shift)
+	print caesar_decrypt(message, shift1)
 
 original_alphabet = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13,
 	"o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25} # alphabet dictionary
